a2499a644da8ac03eb05ae5e6e7bd67677a0b6f9
Seyoon Jeong <seyoonagain@gmail.com>
2024-12-10 16:24:35 +0900

    feat: add font files and configure Tailwind for global usage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        chicago: ['chicago'],
+        geneva: ['geneva'],
+        galmuri7: ['Galmuri7'],
+        galmuri9: ['Galmuri9'],
+        galmuri11: ['Galmuri11'],
+        galmuri14: ['Galmuri14'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
