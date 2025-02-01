9ce9824eacdc972155b805923054ff2a98ffb118
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-02 04:17:48 +0900

    fix: change import of IconProps to default import

--- src/components/common/icon/constants.ts
+++ src/components/common/icon/constants.ts
@@ -1,7 +1,7 @@
 import macintoshHappy from '@public/images/macintoshHappy.svg';
 import linkAddresses from '@public/images/linkAddresses.svg';
 import save from '@public/images/save.svg';
-import { IconProps } from './types';
+import IconProps from './types';
 
 export const ICON_INFO: Array<IconProps> = [
   { title: 'About me', file: macintoshHappy, top: 48, left: 20 },
