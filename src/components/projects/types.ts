e8bc37b90cb7db5ada31c7de4793fdc76ae6630e
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-16 23:31:18 +0900

    types: set project detailed information type

--- src/components/projects/types.ts
+++ src/components/projects/types.ts
@@ -1,1 +1,23 @@
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+
 export type ProjectTitles = '하쿠나마타타로' | '도르멍';
+
+export type ProjectDetails = {
+  title: ProjectTitles;
+  image: StaticImport;
+  startedAt: string;
+  endedAt: string;
+  overview: string;
+  backend: number;
+  frontend: number;
+  techStacks: string[];
+  githubRepo: string;
+  website: string;
+  responsibilities: string[];
+  keyAchievements: string[];
+  lessonsLearned: string[];
+};
+
+export type ProjectDetailsProps = {
+  selectedProject: ProjectTitles;
+};
