560115b3007525118be34308cd4bcc5fe5f5c956
Seyoon Jeong <seyoonagain@gmail.com>
2025-06-03 04:27:46 +0900

    style: change font and content of About Me

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -4,13 +4,13 @@ import ABOUT_ME from '@/components/aboutMe/constants';
 
 const AboutMe = () => {
   return (
-    <div className="flex flex-col items-center gap-6 relative p-5 font-galmuri14">
+    <div className="flex flex-col items-center gap-6 relative p-5">
       <h2 className="font-chicago text-6xl">Hello, world!</h2>
 
-      <h3 className="font-galmuri11 text-base text-center tracking-tighter">
-        말 한마디의 온기가 팀의 성장을 이끈다고 믿는, <br />
+      <h3 className="font-lanaPixel text-xl text-center">
+        직관적 UI와 세심한 UX를 설계하는 <br />
         신입 프론트엔드 개발자
-        <span className="font-galmuri7 text-xl"> 정세윤</span>
+        <span className="text-2xl"> 정세윤</span>
         입니다.
       </h3>
 
@@ -18,9 +18,9 @@ const AboutMe = () => {
         <section key={heading} className="flex flex-col gap-2">
           <div className="flex items-center gap-1">
             <Image src={icon} alt={`${heading} 아이콘`} />
-            <h4 className="font-galmuri9 text-base">{heading}</h4>
+            <h4 className="font-lanaPixel text-base">{heading}</h4>
           </div>
-          <div className="p-2 border border-r-2 border-b-2 border-zinc-950 rounded-md font-galmuri14 text-xs text-justify tracking-tight leading-7">
+          <div className="p-2 border border-r-2 border-b-2 border-zinc-950 rounded-md font-silver text-xl text-justify tracking-tight leading-7">
             {content.split('\n').map(paragraph => (
               <p key={paragraph} className="text-wrap indent-2">
                 {paragraph}
